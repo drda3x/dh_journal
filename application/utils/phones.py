c922bcf27d67e34cd9995ebc5d56b893f7e4cc51
Нестеров Василий <da3x11@gmail.com>
2015-09-11 15:27:56 +0300

    fix баг с телефонами

--- application/utils/phones.py
+++ application/utils/phones.py
@@ -3,7 +3,7 @@
 
 def get_string_val(num):
     _n = str(num)
-    return '+%s(%s)%s-%s-%s' % (_n[0], _n[1:4], _n[4:7], _n[7:9], _n[9:])
+    return '+%s(%s)%s-%s-%s' % (_n[0], _n[1:4], _n[4:7], _n[7:9], _n[9:]) if len(_n) > 1 else ''
 
 
 def check_phone(val):
