a8f1f11d2e73a842dc53da204446ea0285f281fb
da3x <da3x11@gmail.com>
2015-10-22 22:36:25 +0300

    remove CLUB_CARD_ID

--- application/api.py
+++ application/api.py
@@ -18,7 +18,6 @@ from application.models import Students, Passes, Groups, GroupList, PassTypes, L
 from application.views import group_detail_view
 from application.system_api import get_models
 from application.auth import auth_decorator
-from project.settings import CLUB_CARD_ID
 
 from application.system_api import delete_lessons as _delete_lessons
 
