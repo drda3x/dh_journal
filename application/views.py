044720b168d2b70066bc4cb5e5f566f4c7056401
da3x <da3x11@gmail.com>
2015-11-30 23:39:45 +0300

    Доработан интерфейс админа сампо

--- application/views.py
+++ application/views.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 import datetime
 import re
+from pytz import timezone, UTC
+from project.settings import TIME_ZONE
 from django.shortcuts import render_to_response, redirect
 from auth import check_auth, log_out
 from django.template import RequestContext
@@ -9,7 +11,7 @@ from django.template.context_processors import csrf
 from application.utils.passes import get_color_classes
 from application.utils.groups import get_groups_list, get_group_detail, get_student_lesson_status, get_group_students_list
 from application.utils.date_api import get_month_offset, get_last_day_of_month, MONTH_RUS
-from application.models import Lessons, User, Passes, SampoPasses, SampoPassUsage
+from application.models import Lessons, User, Passes, SampoPasses, SampoPassUsage, SampoPayments
 from application.auth import auth_decorator
 from application.utils.date_api import get_count_of_weekdays_per_interval
 
@@ -295,7 +297,7 @@ def club_cards(request):
     students = [{'id': x['id'], 'list': get_group_students_list(x['id'])} for x in all_groups]
 
     context['date_list'] = map(
-        lambda x: {'key':x, 'label': x.strftime('%d.%m.%Y'), 'val': ' '.join(date_group_list[x])},
+        lambda x: {'key': x, 'label': x.strftime('%d.%m.%Y'), 'val': ' '.join(date_group_list[x])},
         date_group_list
     )
     context['expire'] = (now - datetime.timedelta(days=14)).date()
@@ -342,13 +344,52 @@ def sampo_view(request):
         return actions[action](request)
 
     now = datetime.datetime.now()
-    day_begin = now.replace(hour=0, minute=0, second=0, microsecond=0)
-    day_end = now.replace(hour=23, minute=59, second=59, microsecond=999999)
-    begin_time = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-    end_time = (begin_time + datetime.timedelta(days=32)).replace(day=1, hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=1)
+    day_begin = now.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=UTC)
+    day_end = now.replace(hour=23, minute=59, second=59, microsecond=999999, tzinfo=UTC)
+    begin_time = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0, tzinfo=UTC)
+    end_time = (begin_time + datetime.timedelta(days=32)).replace(day=1, hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(microseconds=1)
 
     context = dict()
-    pass_usages = SampoPassUsage.objects.filter(date__range=(day_begin, day_end)).values_list('sampo_pass_id', flat=True)
-    context['passes'] = map(lambda x: setattr(x, 'used_today', x.id in pass_usages) or x, SampoPasses.objects.select_related('payment').filter(payment__date__range=[begin_time, end_time]))
+    sampo_passes = SampoPasses.objects.select_related('payment')\
+        .filter(payment__date__range=[begin_time, end_time])
+
+    today_payments = SampoPayments.objects.filter(date__range=(day_begin, day_end))
+
+    pass_buffer = []
+
+    for payment in today_payments:
+        _pass = filter(lambda p: p.payment.id == payment.id, sampo_passes)
+        if _pass:
+            pass_buffer.append(int(_pass[0].id))
+            payment.sampo_pass = _pass[0]
+
+    pass_usages = SampoPassUsage.objects.select_related('sampo_pass').filter(date__range=(day_begin, day_end))
+
+    def get_str(elem):
+        if isinstance(elem, SampoPayments):
+            if hasattr(elem, 'sampo_pass'):
+                return '%s %s(%d)' % (elem.sampo_pass.surname, elem.sampo_pass.name, elem.money)
+
+            return str(elem.money)
+
+        else:
+            return '%s %s' % (elem.sampo_pass.surname, elem.sampo_pass.name)
+
+    context['today_payments'] = [
+        {
+            'date': i.date.astimezone(timezone(TIME_ZONE)).strftime('%H:%M'),
+            'payment': get_str(i)
+        }
+        for i in list(pass_usages.exclude(sampo_pass_id__in=pass_buffer)) + list(today_payments)
+    ]
+
+    if context['today_payments']:
+        context['today_payments'].sort(key=lambda x: x['date'])
+        context['today_payments'].reverse()
+
+    context['passes'] = map(
+        lambda x: setattr(x, 'used_today', x.id in pass_usages.values_list('sampo_pass_id', flat=True)) or x,
+        sampo_passes
+    )
     template = 'main_view.html' if not request.user.teacher else 'sampo_full.html'
     return render_to_response(template, context, context_instance=RequestContext(request, processors=[custom_proc]))
