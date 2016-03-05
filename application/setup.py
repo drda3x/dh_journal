8737befe9308479e537bce8c90e5654a44ce1132
da3x <da3x11@gmail.com>
2016-03-05 21:42:29 +0300

    added setup

---
+++ application/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dancehustle-journal',
+      version='1.0',
+      description='OpenShift App',
+      author='Vasily Nesterov',
+      author_email='da3x11@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+     install_requires=['Django==1.8.2', 'pytz==2015.7'],
+     )
