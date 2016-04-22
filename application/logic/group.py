42e75e09e2933c0b23435a89a33c1472d336744b
da3x <da3x11@gmail.com>
2016-04-22 08:46:13 +0300

    fix границы отображения групп

--- application/logic/group.py
+++ application/logic/group.py
@@ -21,13 +21,19 @@ class GroupLogic(object):
         self.orm = Groups.objects.select_related('dance_hall').get(pk=group_id)
         group_start_date = datetime.combine(self.orm.start_date, datetime.min.time())
 
-        try:
-            self.date_1 = max(date, group_start_date)
+        if self.orm.end_datetime:
+            self.date_2 = self.orm.end_datetime
+            self.date_1 = deepcopy(self.date_2).replace(day=1, hour=0, minute=0, second=0, microsecond=0)
 
-        except TypeError:
-            self.date_1 = max(datetime(now.year, now.month, 1), group_start_date)
+        else:
+            try:
+                self.date_1 = max(date, group_start_date)
 
-        self.date_2 = self.orm.end_datetime or get_last_day_of_month(self.date_1).replace(hour=23, minute=59, second=59, microsecond=0)
+            except TypeError:
+                self.date_1 = max(datetime(now.year, now.month, 1), group_start_date)
+
+            self.date_2 = get_last_day_of_month(self.date_1).replace(hour=23, minute=59, second=59, microsecond=0)
+
         self.days = get_count_of_weekdays_per_interval(self.orm.days, self.date_1, self.date_2)
 
     def __getattr__(self, item):
