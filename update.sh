c80bd2c710d8780bd4c22be58130c2c6ffa6804c
Василий Нестеров <da3x11@gmail.com>
2017-01-19 22:46:50 +0300

    Новый скрипт-обновлятель

--- update.sh
+++ update.sh
@@ -8,27 +8,27 @@ find $1 -name *.pyc -delete
 echo Создание архива
 catalog=pwd
 cd $1
-tar -czf $1/app.tar.gz application
+tar -czf $1/beta-app.tar.gz application
 cd $catalog
 
 # Копируем исходники на сервак
 echo Копирование файлов на сервер
-scp $1/app.tar.gz u48649@u48649.netangels.ru:~/buffer/
+scp $1/beta-app.tar.gz u48649@u48649.netangels.ru:~/buffer/
 
 # Перезапись исходников
 echo Распаковка файлов и перезапуск сервера
 ssh u48649@u48649.netangels.ru << EOF
-rm -rf ~/teacher.dancehustle.ru/www/application
-tar -xf ~/buffer/app.tar.gz -C ~/teacher.dancehustle.ru/www
+rm -rf ~/beta.teacher.dancehustle.ru/www/application
+tar -xf ~/buffer/beta-app.tar.gz -C ~/beta.teacher.dancehustle.ru/www
 rm -rf ~/buffer/*
 # Запуск сервера
 source ~/python/bin/activate
-python ~/teacher.dancehustle.ru/www/manage.py migrate
+python ~/beta.teacher.dancehustle.ru/www/manage.py migrate
 pkill -u u48649 -f django-wrapper.fcgi
 exit
 EOF
 
 #Удаляем архив
-rm app.tar.gz
+rm beta-app.tar.gz
 
 echo Обновление прошло
