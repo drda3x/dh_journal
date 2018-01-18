5068b17b6f8b362aa8df738a49e929c2fa3476f9
Василий Нестеров <da3x11@gmail.com>
2018-01-18 09:05:18 +0300

    Немного более утонченный скачиватель базы

--- pull_db.sh
+++ pull_db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+mkdir $PWD/db_dumps
 ssh u48649@u48649.netangels.ru ./scripts/make_db_backup.sh $1
-scp u48649@u48649.netangels.ru:~/backups/db_backup.sql ../db_dumps/
+scp u48649@u48649.netangels.ru:~/backups/db_backup.sql $PWD/db_dumps/
 
 source settings.sh
 mysql -u $DB_USER -p$DB_PASSWORD << EOF
@@ -9,6 +10,8 @@ mysql -u $DB_USER -p$DB_PASSWORD << EOF
 drop database TBTEST;
 create database TBTEST;
 use TBTEST;
-source ../db_dumps/db_backup.sql;
+source $PWD/db_dumps/db_backup.sql;
 
 EOF
+
+rm -rf $PWD/db_dumps
